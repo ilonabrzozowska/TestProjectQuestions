ddedc03546519c12b54dbd80e8909234a6f042e5
Ilona Brzozowska <C:\The Bat!\Mail>
2016-06-08 11:33:17 +0200

    Added overlay to modal

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ $(document).ready(function()
 {
     $('div#profile').dialog({
         autoOpen: false,
-        width: $(window).width() * 0.9,
+        width: $(window).width() * 0.8,
+        modal: true, 
+        open: function() { 
+            $('.ui-widget-overlay').addClass('ui-grey-overlay'); 
+        },
+        close: function() { 
+            $('.ui-widget-overlay').removeClass('ui-grey-overlay'); 
+        },
     }).siblings('.ui-dialog-titlebar').remove();
     
     $('img.photo-circle').click(function() { 
         $('div#profile').dialog('open'); 
-
     });
     
     $('button.close-button').click(function() { 
         $('div#profile').dialog('close'); 
-
     });
 });
